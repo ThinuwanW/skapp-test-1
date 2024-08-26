76c209ed08332e217b9ce90b5d8e102e0c65c7a1
ThinuwanW <thinuwan.wickramaarachchi@rootcode.io>
2024-08-26 19:22:44 +0530

    added submodule

--- pages/employees.js
+++ pages/employees.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import LeaveTrends from "@/enterprise/LeaveTrends";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,7 @@ export default function Employees() {
         <br/>
         <h2>Name: Eric</h2>
         <h3>Job: Finance manager</h3>
-        <h3>Career Progression: Good</h3>
+        <LeaveTrends/>
     </div>
   );
 }
